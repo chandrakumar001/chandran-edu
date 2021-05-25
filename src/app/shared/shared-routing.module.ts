df5840c4e94d6aba1d0a04ac4c7b167510d5de28
Chandra Kumar <osaimar19@gmail.com>
2021-05-25 08:03:35 +0530

    Added constant and img assets

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//header-component
+import { HeaderComponent } from '@app/shared/header/header.component';
+import { RightNavComponent } from '@app/shared/header/right-nav/right-nav.component';
+import { CompanyLogoComponent } from '@app/shared/header/company-logo/company-logo.component';
+import { DynamicMenuComponent } from '@app/shared/Component/dynamic-menu/dynamic-menu.component';
+import { AnnouncementComponent } from '@app/shared/header/announcement/announcement.component';
+// footer-component
+import { FooterComponent } from '@app/shared/footer/footer.component';
+import { SocialLinksComponent } from '@app/shared/footer/social-links/social-links.component';
+import { CompanyProfilesComponent } from '@app/shared/footer/company-profiles/company-profiles.component';
+import { CopyrightComponent } from '@app/shared/footer/copyright/copyright.component';
+
 const routes: Routes = [];
 
 @NgModule({
@@ -8,3 +20,17 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class SharedRoutingModule { }
+
+export const SHARED_COMPONENTS = [
+  //header
+  HeaderComponent,
+  RightNavComponent,
+  CompanyLogoComponent,
+  //footer
+  FooterComponent,
+  SocialLinksComponent,
+  CompanyProfilesComponent,
+  CopyrightComponent,
+  DynamicMenuComponent,
+  AnnouncementComponent
+]
